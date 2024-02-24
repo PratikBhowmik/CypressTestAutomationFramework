0d4cb1bd52adbd11c5b8446f8f50088e2dc73de6
Pratik <impratikbhowmik@gmail.com>
2024-02-24 06:58:28 +0530

    changes committed

--- cypress/e2e/tests/Web3Tests.cy.js
+++ cypress/e2e/tests/Web3Tests.cy.js
@@ -5,24 +5,36 @@ beforeEach(() => {
 
 describe("Web3 test cases",() => {
 
-    it ('Verify clicking on news redirect to news page on top', () => {
-        cy.viewport('iphone-se2');
+    it ('tc_web3test_01', () => {
+        // cy.viewport('iphone-se2');
         cy.get('[class = "px-2 text-sm md:text-base hover:text-white transition-all hover:ease-in-out duration-300"]').click();
         cy.wait(3000);
         cy.get('[class = " flex w-full items-center justify-between pb-4"]').should('contain.text','What\'s Trending');
     })
 
-    it ('Verify clicking on news redirect to news page on down', () => {
-        cy.viewport('iphone-se2');
+    it ('tc_web3test_02', () => {
+        // cy.viewport('iphone-se2');
         cy.scrollTo(0,1000);
         cy.get("[class = \"flex items-center gap-2 \"]").contains("News").click();
+        cy.wait(3000);
         cy.get('[class = " flex w-full items-center justify-between pb-4"]').should('contain.text','What\'s Trending');
     })
 
-    it ('Verify clicking on learn redirects to learn page', () => {
-        cy.viewport('iphone-se2');
+    it ('tc_web3test_03', () => {
+        // cy.viewport('iphone-se2');
         cy.get("[class = \"fixed z-[9999] flex justify-between w-full px-8 py-2 top-4 font-poppins\"]").contains("Learn").click();
+        cy.wait(3000);
         cy.url().should('include','app/learn');
+    })
+
+    it('tc_web3test_04', () => {
+        cy.get("[class = \"fixed z-[9999] flex justify-between w-full px-8 py-2 top-4 font-poppins\"]").contains("Earn").click();
+        cy.wait(3000);
+        cy.url().should("include", "app/#earn");
+    })
+
+    it ('tc_web3test_05', () => {
+        cy.get("[class = \"fixed z-[9999] flex justify-between w-full px-8 py-2 top-4 font-poppins\"]").should("contain.text","HOW3");
     })
 
 })
